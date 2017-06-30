3cb4a44cff0517b013c6883c9592f6ae1d9cef14
Tom Randolph <tom.randolph@equipmentshare.com>
2017-06-29 22:02:47 -0500

    Persist menu state

--- src/js/views/chrome.js
+++ src/js/views/chrome.js
@@ -1,6 +1,7 @@
 import Ractive from "ractive";
 
 import Templates from "../Templates.js";
+import Storage from "../Storage.js";
 
 var ChromeView = Ractive.extend( {
     "template": Templates.getView( "chrome" ),
@@ -12,13 +13,28 @@ var ChromeView = Ractive.extend( {
     },
 
     "on": {
+        init(){
+            this.loadState();
+        },
         flex(){
             this.toggle( "collapsed" );
+
+            Storage.set( "strongbox.menus.main.collapsed", this.get( "collapsed" ) );
         }
     },
 
     setMenu( newSelection = "home" ){
         this.set( "selected", newSelection );
+
+        Storage.set( "strongbox.menus.main.active", newSelection );
+    },
+    loadState(){
+        var menu = Storage.get( "strongbox.menus.main" );
+
+        this.set( {
+            "collapsed": menu && menu.collapsed || false,
+            "selected": menu && menu.active || "home"
+        } );
     }
 } );
 
