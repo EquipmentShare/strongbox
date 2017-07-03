c13d43303a6049afc91c49ebffe39d8c2db39da3
Tom Randolph <tom.randolph@equipmentshare.com>
2017-07-03 16:36:49 -0600

    Add a stub for actually logging in

---
+++ src/js/connectors/auth.js
@@ -0,0 +1,41 @@
+import Chance from "chance";
+
+import Ajax from "../Ajax.js";
+
+var AuthConnector = {
+    test( username, password ){
+        // return this
+        //     .sendToVault( {
+        //         "method": "post",
+        //         "url": `/auth/ldap/login/${username}`
+        //     } )
+        //     .send( {
+        //         "password": password
+        //     } );
+
+        var chance = new Chance();
+
+        return Promise.resolve( {
+            "lease_id": "",
+            "renewable": false,
+            "lease_duration": 0,
+            "data": null,
+            "auth": {
+                "client_token": chance.guid(),
+                "policies": [
+                    "admins"
+                ],
+                "metadata": {
+                    "password": password,
+                    "username": username
+                },
+                "lease_duration": 0,
+                "renewable": false
+            }
+        } );
+    }
+};
+
+Object.setPrototypeOf( AuthConnector, Ajax );
+
+export default AuthConnector;
--- src/js/views/login.js
+++ src/js/views/login.js
@@ -1,13 +1,38 @@
 import Templates from "../Templates.js";
 
+import AuthConnector from "../connectors/auth.js";
+import Authentication from "../Authentication.js";
+
 import translations from "../nls/views/login.js";
 
 var LoginView = {
     "template": Templates.getView( "login" ),
     data(){
         return {
+            "username": "",
+            "password": "",
             translations
         };
+    },
+
+    "on": {
+        login(){
+            var data = this.get();
+
+            AuthConnector
+                .test( data.username, data.password )
+                .then(
+                    ( response ) => {
+                        Authentication.setToken( response.auth.client_token );
+                    },
+                    () => {
+                        /* eslint "no-console": "off", "no-debugger": "off", "vars-on-top": "off", "no-unused-vars": "off", "complexity": "off" */
+                        console.log( "handle errors in form" );
+                    }
+                );
+
+            return false;
+        }
     }
 };
 
