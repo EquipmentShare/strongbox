33ab7c2469b0d670aa22ae8b983e3e7ca85f34cf
Tom Randolph <tom.randolph@equipmentshare.com>
2017-07-05 18:48:43 -0600

    Add rough stub of showing secrets based on dynamic data

---
+++ src/js/connectors/generic.js
@@ -0,0 +1,93 @@
+import Ajax from "../Ajax.js";
+
+var AuthConnector = {
+    read( path = "" ){
+        // return this.
+        //     sendToVault( {
+        //         "method": "get",
+        //         "url": `/secret/${path}`
+        //     } );
+
+        var payloads = {
+            "foo": {
+                "prop": "foo"
+            },
+            "keys/stuff": {
+                "prop": "stuff"
+            },
+            "keys/things": {
+                "prop": "things"
+            },
+            "keys/more/friend": {
+                "prop": "friend"
+            },
+            "keys/hello/there": {
+                "prop": "there"
+            }
+        };
+        var wrapper = {
+            "auth": null,
+            "data": null,
+            "lease_duration": 2764800,
+            "lease_id": "",
+            "renewable": false
+        };
+        var response;
+
+        if( payloads[ path ] ){
+            wrapper.data = payloads[ path ];
+            response = Promise.resolve( wrapper );
+        }
+        else{
+            response = Promise.reject();
+        }
+
+        return response;
+    },
+    list( path = "" ){
+        /* eslint "complexity": "off" */
+
+        // return this
+        //     .sendToVault( {
+        //         "method": "list",
+        //         "url": `/secret/${path}`
+        //     } );
+
+        var payloads = {
+            "default": {
+                "keys": [ "foo", "keys/" ]
+            },
+            "keys/": {
+                "keys": [ "hello/", "stuff", "things", "more/" ]
+            },
+            "keys/hello/": {
+                "keys": [ "there" ]
+            },
+            "keys/more/": {
+                "keys": [ "friend" ]
+            }
+        };
+        var wrapper = {
+            "auth": null,
+            "data": null,
+            "lease_duration": 2764800,
+            "lease_id": "",
+            "renewable": false
+        };
+        var response;
+
+        if( payloads[ path ] || path == "" ){
+            wrapper.data = payloads[ path ] || payloads.default;
+            response = Promise.resolve( wrapper );
+        }
+        else{
+            response = Promise.reject();
+        }
+
+        return response;
+    }
+};
+
+Object.setPrototypeOf( AuthConnector, Ajax );
+
+export default AuthConnector;
--- src/js/views/secrets.js
+++ src/js/views/secrets.js
@@ -1,5 +1,6 @@
 import Templates from "../Templates.js";
 
+import GenericConnector from "../connectors/generic.js";
 import Icon from "../components/Icon.js";
 
 import translations from "../nls/views/secrets.js";
@@ -9,11 +10,60 @@ var SecretsView = {
     data(){
         return {
             "visible": false,
+            "secrets": {
+                "secret": []
+            },
             translations
         };
     },
     "components": {
         "icon": Icon
+    },
+
+    "on": {
+        init(){
+            this.getSecrets();
+        }
+    },
+
+    getSecrets( path = "" ){
+        GenericConnector
+            .list( path )
+            .then( ( response ) => {
+                if( response.data.keys ){
+                    response.data.keys.forEach( ( key ) => {
+                        let isPath = key.slice( -1 ) == "/";
+                        let resp;
+
+                        if( isPath ){
+                            resp = GenericConnector.list( path + key );
+                            this.getSecrets( path + key );
+                        }
+                        else{
+                            resp = GenericConnector.read( path + key );
+                        }
+
+                        resp.then( ( result ) => {
+                            this.push(
+                                "secrets.secret",
+                                {
+                                    "path": path + key,
+                                    "data": result.data
+                                }
+                            );
+                        } );
+                    } );
+                }
+                else{
+                    this.push(
+                        "secrets.secret",
+                        {
+                            "path": path,
+                            "data": response.data
+                        }
+                    );
+                }
+            } );
     }
 };
 
