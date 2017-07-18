baca8ee29f9e5714c2c38402eb1b05e2f0cbfc5c
Tom Randolph <tom.randolph@equipmentshare.com>
2017-07-18 17:03:58 -0600

    Switch to FontAwesome5 SVG framework

--- src/js/components/Icon.js
+++ src/js/components/Icon.js
@@ -1,4 +1,7 @@
 import Ractive from "ractive";
+import "../../static/fontawesome/js/fontawesome.js";
+import "../../static/fontawesome/js/packs/brands.js";
+import "../../static/fontawesome/js/packs/light.js";
 
 import Templates from "../Templates.js";
 
@@ -9,6 +12,22 @@ var Icon = Ractive.extend( {
             "icon": "",
             "type": "light"
         };
+    },
+    "computed": {
+        weight(){
+            var type = this.get( "type" );
+            var weights = {
+                "light": "fal",
+                "brand": "fab"
+            };
+            var weight = "fal";
+
+            if( weights[ type ] ){
+                weight = weights[ type ];
+            }
+
+            return weight;
+        }
     }
 } );
 
