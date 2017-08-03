ddda8b13b8de02eb7b7b8050b3dca6310bc328a1
Tom Randolph <tom.randolph@equipmentshare.com>
2017-08-02 21:52:08 -0600

    Collect more configuration details on setup

--- setup.js
+++ setup.js
@@ -9,22 +9,50 @@ var inquirer = require( "inquirer" );
 var fs = require( "fs" );
 var path = require( "path" );
 
+var fullSeparator = "-----------------------------------";
+
 var questions = [
     {
         "name": "endpoint",
         "type": "input",
         "message": "Enter the vault instance this app will connect to: "
+    },
+    {
+        "name": "secrets",
+        "type": "checkbox",
+        "message": "Choose which secret backends to enable: ",
+        "default": [ "generic" ],
+        "choices": [ "generic" ]
+    },
+    {
+        "name": "auths",
+        "type": "checkbox",
+        "message": "Choose which auth backends to enable: ",
+        "default": [ "LDAP" ],
+        "choices": [ "LDAP" ]
     }
 ];
+
+function secretsFollowup( backend ){
+    var mounts = {
+        "generic": "secret/"
+    };
+
+    return {
+        "name": `${backend}`,
+        "type": "input",
+        "message": `Enter the mount point for the ${backend} secret backend: `,
+        "default": mounts[ backend ] || ""
+    };
+}
 
 function fixEndpoint( endpoint ){
     var pre = endpoint;
 
     if( endpoint.substr( -1 ) == "/" ){
         endpoint = endpoint.slice( 0, -1 );
+        console.log( chalk.yellow( `\t- Got '${pre}', normalizing to '${endpoint}'` ) );
     }
-
-    console.log( chalk.yellow( `\t- Got '${pre}', normalizing to '${endpoint}'` ) );
 
     return endpoint;
 }
@@ -33,7 +61,9 @@ function fixResponses( responses ){
     console.log( chalk.green( "Checking for some potential problems..." ) );
 
     return {
-        "vault": fixEndpoint( responses.endpoint )
+        "vault": fixEndpoint( responses.endpoint ),
+        "secrets": responses.secrets,
+        "auths": responses.auths
     };
 }
 
@@ -68,5 +98,21 @@ console.log( // eslint-disable-line
 inquirer
     .prompt( questions )
     .then( ( responses ) => {
-        writeConfigToApp( fixResponses( responses ) );
+        let mounts = responses.secrets.map( secretsFollowup );
+
+        console.log( fullSeparator );
+
+        inquirer
+            .prompt( mounts )
+            .then( ( mountPoints ) => {
+                let merged = {};
+
+                responses.secrets.forEach( ( backend ) => {
+                    merged[ backend ] = mountPoints[ backend ];
+                } );
+
+                responses.secrets = merged;
+
+                writeConfigToApp( fixResponses( responses ) );
+            } );
     } );
