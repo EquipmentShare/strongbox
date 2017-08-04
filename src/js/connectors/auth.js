5504fb82fabc8b3bf5ae11e433f9b7539b3889bb
Tom Randolph <tom.randolph@equipmentshare.com>
2017-08-04 12:54:07 -0600

    Correct connection to vault

--- src/js/connectors/auth.js
+++ src/js/connectors/auth.js
@@ -1,38 +1,15 @@
-import Chance from "chance";
-
 import Ajax from "../Ajax.js";
 
 var AuthConnector = {
     ldapLogin( username, password ){
-        // return this
-        //     .sendToVault( {
-        //         "method": "post",
-        //         "url": `/auth/ldap/login/${username}`
-        //     } )
-        //     .send( {
-        //         "password": password
-        //     } );
-
-        var chance = new Chance();
-
-        return Promise.resolve( {
-            "lease_id": "",
-            "renewable": false,
-            "lease_duration": 0,
-            "data": null,
-            "auth": {
-                "client_token": chance.guid(),
-                "policies": [
-                    "admins"
-                ],
-                "metadata": {
-                    "password": password,
-                    "username": username
-                },
-                "lease_duration": 0,
-                "renewable": false
-            }
-        } );
+        return this
+            .sendToVault( {
+                "method": "post",
+                "url": `/auth/ldap/login/${username}`,
+                "data": {
+                    "password": password
+                }
+            } );
     }
 };
 
--- src/js/connectors/generic.js
+++ src/js/connectors/generic.js
@@ -1,110 +1,37 @@
 import Ajax from "../Ajax.js";
 
-// const PREFIX = "secret/";
+const PREFIX = "secret";
 var GenericConnector;
 
-// function stripPrefix( path ){
-//     var parts;
-//
-//     if( typeof path == "string" ){
-//         parts = path.split( "/" );
-//
-//         if( parts[ 0 ] == PREFIX ){
-//             path = parts.slice( 1 ).join( "/" );
-//         }
-//         else{
-//             path = parts.join( "/" );
-//         }
-//     }
-//
-//     return path;
-// }
+function stripPrefix( path ){
+    var parts;
+
+    if( typeof path == "string" ){
+        parts = path.split( "/" );
+
+        if( parts[ 0 ] == PREFIX ){
+            path = parts.slice( 1 ).join( "/" );
+        }
+        else{
+            path = parts.join( "/" );
+        }
+    }
+
+    return path;
+}
 
 GenericConnector = {
     read( path = "" ){
-        // return this.
-        //     sendToVault( {
-        //         "method": "get",
-        //         "url": `/secret/${stripPrefix( path )}`
-        //     } );
-
-        var payloads = {
-            "secret/foo": {
-                "prop": "foo"
-            },
-            "secret/keys/stuff": {
-                "prop": "stuff"
-            },
-            "secret/keys/things": {
-                "prop": "things"
-            },
-            "secret/keys/more/friend": {
-                "prop": "friend"
-            },
-            "secret/keys/hello/there": {
-                "prop": "there"
-            }
-        };
-        var wrapper = {
-            "auth": null,
-            "data": null,
-            "lease_duration": 2764800,
-            "lease_id": "",
-            "renewable": false
-        };
-        var response;
-
-        if( payloads[ path ] ){
-            wrapper.data = payloads[ path ];
-            response = Promise.resolve( wrapper );
-        }
-        else{
-            response = Promise.reject();
-        }
-
-        return response;
+        return this.sendToVault( {
+            "method": "get",
+            "url": `/secret/${stripPrefix( path )}`
+        } );
     },
     list( path = "" ){
-        /* eslint "complexity": "off" */
-
-        // return this
-        //     .sendToVault( {
-        //         "method": "list",
-        //         "url": `/secret/${stripPrefix( path )}`
-        //     } );
-
-        var payloads = {
-            "default": {
-                "keys": [ "foo", "keys/" ]
-            },
-            "secret/keys/": {
-                "keys": [ "hello/", "stuff", "things", "more/" ]
-            },
-            "secret/keys/hello/": {
-                "keys": [ "there" ]
-            },
-            "secret/keys/more/": {
-                "keys": [ "friend" ]
-            }
-        };
-        var wrapper = {
-            "auth": null,
-            "data": null,
-            "lease_duration": 2764800,
-            "lease_id": "",
-            "renewable": false
-        };
-        var response;
-
-        if( payloads[ path ] || path == "" || path == "secret" || path == "secret/" ){
-            wrapper.data = payloads[ path ] || payloads.default;
-            response = Promise.resolve( wrapper );
-        }
-        else{
-            response = Promise.reject();
-        }
-
-        return response;
+        return this.sendToVault( {
+            "method": "list",
+            "url": `/secret/${stripPrefix( path )}`
+        } );
     }
 };
 
